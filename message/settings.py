b65b504a5c2641df78e31830110a7f416ea1f6ea
AngelVOC <105071872+AngelVEC@users.noreply.github.com>
2024-06-13 11:16:55 +1200

    add requirement.txt and connect the database

--- message/settings.py
+++ message/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'message.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'NjVTxWOF4et5',
+        'HOST': 'ep-ancient-bird-a7cfx35f-pooler.ap-southeast-2.aws.neon.tech',
+        'PORT': '5432',
     }
 }
 
