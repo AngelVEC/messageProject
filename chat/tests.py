ded1f5d1048dfb33ad694fc3cf5fa33bd2e49be6
AngelVOC <105071872+AngelVEC@users.noreply.github.com>
2024-06-24 21:54:05 +1200

    fix some error

--- chat/tests.py
+++ chat/tests.py
@@ -1,50 +1,50 @@
 from django.test import TestCase
-
-# Create your tests here.
-# tests.py
-from django.urls import reverse
+from rest_framework.test import APIClient
 from rest_framework import status
-from rest_framework.test import APITestCase
 from .models import ChatRoom
 from .serializers import ChatRoomSerializer
+from django.urls import reverse
 
-class ChatRoomViewSetTest(APITestCase):
+
+class ChatRoomViewSetTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Set up data for the whole TestCase
+        cls.chatroom1 = ChatRoom.objects.create(name='Test Room 1')
+        cls.chatroom2 = ChatRoom.objects.create(name='Test Room 2')
+
     def setUp(self):
-        # 创建一些测试数据
-        self.chat_room1 = ChatRoom.objects.create(name='Room 1')
-        self.chat_room2 = ChatRoom.objects.create(name='Room 2')
-        self.valid_payload = {'name': 'New Room'}
-        self.invalid_payload = {'name': ''}
+        self.client = APIClient()
 
-    def test_get_all_chatrooms(self):
+    def test_list_chatrooms(self):
         response = self.client.get(reverse('chatroom-list'))
         chatrooms = ChatRoom.objects.all()
         serializer = ChatRoomSerializer(chatrooms, many=True)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
 
-    def test_create_valid_chatroom(self):
-        response = self.client.post(reverse('chatroom-list'), data=self.valid_payload)
+    def test_create_chatroom(self):
+        data = {'name': 'New Test Room'}
+        response = self.client.post(reverse('chatroom-list'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(ChatRoom.objects.count(), 3)
+        self.assertEqual(ChatRoom.objects.get(id=response.data['id']).name, 'New Test Room')
 
-    def test_create_invalid_chatroom(self):
-        response = self.client.post(reverse('chatroom-list'), data=self.invalid_payload)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-
-    def test_get_valid_single_chatroom(self):
-        response = self.client.get(reverse('chatroom-detail', kwargs={'pk': self.chat_room1.pk}))
-        chatroom = ChatRoom.objects.get(pk=self.chat_room1.pk)
+    def test_retrieve_chatroom(self):
+        response = self.client.get(reverse('chatroom-detail', kwargs={'pk': self.chatroom1.pk}))
+        chatroom = ChatRoom.objects.get(pk=self.chatroom1.pk)
         serializer = ChatRoomSerializer(chatroom)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, serializer.data)
 
     def test_update_chatroom(self):
-        response = self.client.put(reverse('chatroom-detail', kwargs={'pk': self.chat_room1.pk}),
-                                   data={'name': 'Updated Room'})
+        data = {'name': 'Updated Test Room'}
+        response = self.client.put(reverse('chatroom-detail', kwargs={'pk': self.chatroom1.pk}), data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data['name'], 'Updated Room')
+        self.chatroom1.refresh_from_db()
+        self.assertEqual(self.chatroom1.name, 'Updated Test Room')
 
     def test_delete_chatroom(self):
-        response = self.client.delete(reverse('chatroom-detail', kwargs={'pk': self.chat_room1.pk}))
+        response = self.client.delete(reverse('chatroom-detail', kwargs={'pk': self.chatroom1.pk}))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(ChatRoom.objects.filter(pk=self.chat_room1.pk).exists())
+        self.assertEqual(ChatRoom.objects.count(), 1)
