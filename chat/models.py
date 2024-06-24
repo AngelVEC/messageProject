7083c96dc3b24f719801ac7f72f82879974e049f
AngelVOC <105071872+AngelVEC@users.noreply.github.com>
2024-06-24 21:33:59 +1200

    news

--- chat/models.py
+++ chat/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class ChatRoom(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
@@ -15,7 +15,7 @@ class Message(models.Model):
     text = models.TextField()
     ChatRoom = models.ForeignKey(ChatRoom, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
