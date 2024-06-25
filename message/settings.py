bbd5c2d6a0327775ac8cf8047217366307d3b456
AngelVOC <105071872+AngelVEC@users.noreply.github.com>
2024-06-26 10:50:23 +1200

    Update settings.py

--- message/settings.py
+++ message/settings.py
@@ -104,23 +104,23 @@ WSGI_APPLICATION = 'message.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'verceldb',
-        'USER': 'default',
-        'PASSWORD': 'DcmZ3rSPTQ5M',
-        'HOST': 'ep-blue-voice-a4cy5248.us-east-1.aws.neon.tech',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'verceldb',
+#         'USER': 'default',
+#         'PASSWORD': 'DcmZ3rSPTQ5M',
+#         'HOST': 'ep-blue-voice-a4cy5248.us-east-1.aws.neon.tech',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
