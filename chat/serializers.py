fd1e2438a5e6f76c4afcd570ad8329f2acc0fc8c
AngelVOC <105071872+AngelVEC@users.noreply.github.com>
2024-07-11 12:45:24 +1200

    register API

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,5 +1,8 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from chat.models import ChatRoom, Message
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+
 
 # for API
 class ChatRoomSerializer(serializers.ModelSerializer):
@@ -12,3 +15,16 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
+
+class userRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['all']
+
+    def create(self, clean_data):
+        if User.objects.filter(email=clean_data['email']).exists() or User.objects.filter(username=clean_data['username']).exists():
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+        user_obj = User.objects.create_user(email=clean_data['email'], username = clean_data['username'], password= clean_data['password'])
+        user_obj.save()
+        return Response(status=status.HTTP_201_CREATED)
--- chat/viewsets.py
+++ chat/viewsets.py
@@ -1,7 +1,8 @@
 from chat.models import ChatRoom, Message
 from chat.serializers import ChatRoomSerializer,MessageSerializer
-from rest_framework import viewsets
-
+from rest_framework import viewsets, permissions
+from rest_framework.views import APIView
+from chat.serializers import userRegisterSerializer
 
 class ChatRoomViewSet(viewsets.ModelViewSet):
     queryset = ChatRoom.objects.all()
@@ -10,3 +11,12 @@ class ChatRoomViewSet(viewsets.ModelViewSet):
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
+
+class registerView(APIView) :
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        clean_data = request.data
+        serializer = userRegisterSerializer(data=clean_data)
+
+        return serializer.create(clean_data)
