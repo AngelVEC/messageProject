a203eb0460ca138818dca6f73bc49588d156a2ed
Lei <gabriel_sl19798@hotmail.com>
2024-07-15 16:49:11 +1200

    I changed the model, add created_by and members in chatroom

--- chat/models.py
+++ chat/models.py
@@ -5,8 +5,10 @@ from django.contrib.auth.models import User
 
 class ChatRoom(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    created = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    members = models.ManyToManyField(User, related_name="chat_room_members")
 
     def __str__(self):
         return self.name
